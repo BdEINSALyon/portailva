31b188490c05e3f2be508ae33cfcc23395563b57
Léo Martinez <leo84.martinez@gmail.com>
2016-11-15 13:18:14 +0100

    [API] Correct /events/ endpoint

--- portailva/api_urls.py
+++ portailva/api_urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     # API v1
     url(r'^v1/directory/', include('portailva.directory.api_v1.urls')),
-    url(r'^v1/event/', include('portailva.event.api_v1.urls'))
+    url(r'^v1/events/', include('portailva.event.api_v1.urls'))
 ]
