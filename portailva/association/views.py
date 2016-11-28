6f9350dfe2e222be4c883b09266b85a85d4cd2ad
Léo Martinez <leo84.martinez@gmail.com>
2016-11-28 15:32:12 +0100

    Fix wrong permission check on association mandate list view

--- portailva/association/views.py
+++ portailva/association/views.py
@@ -152,11 +152,6 @@ class AssociationMandateListView(AssociationMixin, ListView):
     model = Mandate
     template_name = 'association/mandate/list.html'
 
-    def get(self, request, *args, **kwargs):
-        if not request.user.has_perm('association.can_admin_mandate'):
-            raise PermissionDenied
-        return super(AssociationMandateListView, self).get(request, *args, **kwargs)
-
     def get_context_data(self, **kwargs):
         context = super(AssociationMandateListView, self).get_context_data(**kwargs)
         return context
