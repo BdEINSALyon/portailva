0016262614f197a68116a665e2097e9b9dfb932a
Philippe Vienne <philippegeek@gmail.com>
2017-01-05 13:06:13 -0800

    Writing views

--- portailva/newsletter/urls.py
+++ portailva/newsletter/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    #url(r'^$', LoginView.as_view(), name='member-login'),
+    url('^association/(?P<association_pk>\d+)/article/$', AssociationarticleListView.as_view(),
+        name='association-article-list'),
+    url('^association/(?P<association_pk>\d+)/article/new/$', AssociationarticleNewView.as_view(),
+        name='association-article-new'),
+    url('^association/(?P<association_pk>\d+)/article/(?P<pk>\d+)/$', AssociationarticleDetailView.as_view(),
+        name='association-article-detail'),
+    url('^association/(?P<association_pk>\d+)/article/(?P<pk>\d+)/update/$', AssociationarticleUpdateView.as_view(),
+        name='association-article-update'),
+    url('^association/(?P<association_pk>\d+)/article/(?P<pk>\d+)/delete/$', AssociationarticleDeleteView.as_view(),
+        name='association-article-delete'),
 ]
--- portailva/newsletter/views.py
+++ portailva/newsletter/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-# Create your views here.
+from portailva.association.mixins import AssociationMixin
+from portailva.newsletter.models import Article
+
+
+class AssociationArticleListArticleView(AssociationMixin, ListView):
+    model = Article
+    template_name = 'newsletter/article/.html'
+
+
+class AssociationArticleNewArticleView(AssociationMixin, CreateView):
+    model = Article
+    template_name = 'newsletter/article/.html'
+
+
+class AssociationArticleUpdateArticleView(AssociationMixin, UpdateView):
+    model = Article
+    template_name = 'newsletter/article/.html'
+
+
+class AssociationArticleDetailedView(AssociationMixin, DetailView):
+    model = Article
+    template_name = 'newsletter/article/.html'
+
+
+class AssociationArticleDeleteView(AssociationMixin, DeleteView):
+    model = Article
