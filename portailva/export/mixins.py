9d643cf751e966744dbb6795547e282d6a6ae714
Philippe Vienne <philippegeek@gmail.com>
2017-03-16 16:46:22 +0100

    Edit permissions and add navbar link

--- portailva/export/mixins.py
+++ portailva/export/mixins.py
@@ -1,6 +1,9 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, UserPassesTestMixin, AccessMixin
 
 
-class AbleToExportMixin(PermissionRequiredMixin):
-    permission_required = 'export.use'
-    permission_denied_message = "Vous n'avez pas les permissions nécessaires pour exporter des données."
+class AbleToExportMixin(AccessMixin):
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.is_staff:
+            return self.handle_no_permission()
+        return super(AbleToExportMixin, self).dispatch(request, *args, **kwargs)
