a642b021314415c752f685f19737d46d915a6b06
Philippe Vienne <philippegeek@gmail.com>
2017-03-22 09:07:42 +0100

    Add documentation for FolderScoped

--- portailva/file/views/admin_resources.py
+++ portailva/file/views/admin_resources.py
@@ -11,25 +11,46 @@ from portailva.file.models import ResourceFolder, ResourceFile, FileVersion
 
 
 class FolderScoped(View):
+    """
+    Define a view where the path argument may contains a folder.
+    The path argument "folder_pk" is used as the id of current folder.
+    The class contains useful functions to retrieve current folder, files and sub-folders.
+    """
     def _get_folder(self):
+        """Get current folder or None if there is not"""
         if self.kwargs['folder_pk'] is not None:
             return ResourceFolder.objects.get(pk=self.kwargs['folder_pk'])
         else:
             return None
 
     def _get_files(self):
+        """
+        List files contained into the current directory.
+        If the current directory is None, it list files on root directory.
+        A file on root directory is a file without folder.
+        """
         if self.kwargs['folder_pk'] is not None:
             return self._get_folder().resources.all()
         else:
             return ResourceFile.objects.filter(folder=None)
 
     def _get_folders(self):
+        """
+        List folders contained into the current directory.
+        If the current directory is None, it list folders on root directory.
+        A folder on root directory is a file without any parent.
+        """
         if self.kwargs['folder_pk'] is not None:
             return self._get_folder().children.all()
         else:
             return ResourceFolder.objects.filter(parent=None)
 
     def url(self):
+        """
+        Generate URL to the current folder.
+        It can be used to redirect the user to the current folder after a form or deletion.
+        :return: the string path computed by reverse django method
+        """
         folder = self._get_folder()
         if folder is None:
             return reverse('resource-folder-list')
@@ -136,27 +157,3 @@ class ResourceFileDeleteView(LoginRequiredMixin, FolderScoped, DeleteView):
 
     def get_success_url(self):
         return self.url()
-
-
-class ResourceFolderCreateView(LoginRequiredMixin, CreateView):
-    http_method_names = ['get', 'post']
-    form_class = ResourceFolderForm
-    current_folder = None
-    template_name = 'file/resources/folders/create.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.has_perm('file.manage_resources'):
-            raise PermissionDenied
-        return super(ResourceFolderCreateView, self).dispatch(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        form = self.get_form(self.form_class)
-        if form.is_valid():
-            return self.form_valid(form)
-        return render(request, self.template_name, {
-            'form': form
-        })
-
-    def form_valid(self, form):
-        form.save()
-        return redirect(reverse('resource-folder-list'))
