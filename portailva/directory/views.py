59674159bd38f2dacdef79be596aee825c12b536
Gabriel Augendre <gabriel@augendre.info>
2017-10-13 16:24:08 +0200

    Add search capability over name, acronym, category, place and description

--- portailva/directory/views.py
+++ portailva/directory/views.py
@@ -1,6 +1,8 @@
+import re
 from datetime import datetime
 
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -194,11 +196,66 @@ class AssociationDirectoryPublicView(ListView):
     template_name = 'directory/public.html'
     model = Association
     context_object_name = 'associations'
+    query = None
     queryset = (Association.objects
                 .filter(is_active=True)
                 .filter(directory_entries__isnull=False)
                 .filter(directory_entries__is_online=True)
                 .distinct())
+
+    @staticmethod
+    def normalize_query(query_string,
+                        findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                        normspace=re.compile(r'\s{2,}').sub):
+        """ Splits the query string in invidual keywords, getting rid of unecessary spaces
+            and grouping quoted words together.
+            Example:
+
+            >>> AssociationDirectoryPublicView.normalize_query('  some random  words "with   quotes  " and   spaces')
+            ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+
+        """
+        return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+    @staticmethod
+    def get_query(query_string, search_fields):
+        """Returns a query, that is a combination of Q objects. That combination
+            aims to search keywords within a model by testing the given search fields.
+        """
+        query = None  # Query to search for every search term
+        terms = AssociationDirectoryPublicView.normalize_query(query_string)
+        for term in terms:
+            or_query = None  # Query to search for a given term in each field
+            for field_name in search_fields:
+                q = Q(**{"%s__unaccent__icontains" % field_name: term})
+                if or_query is None:
+                    or_query = q
+                else:
+                    or_query |= q
+            if query is None:
+                query = or_query
+            else:
+                query &= or_query
+        return query
+
+    def dispatch(self, request, *args, **kwargs):
+        self.query = self.request.GET.get('query')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        if self.query:
+            return (Association.objects
+                    .filter(is_active=True)
+                    .filter(directory_entries__isnull=False)
+                    .filter(directory_entries__is_online=True)
+                    .filter(self.get_query(self.query, ['name', 'acronym',
+                                                        'category__name',
+                                                        'directory_entries__description',
+                                                        'directory_entries__place__name',
+                                                        ]))
+                    .distinct())
+        else:
+            return AssociationDirectoryPublicView.queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
