f96917e5a85ae25c3290598f69d2ac9a4c4509cf
Gabriel Augendre <gabriel@augendre.info>
2017-10-13 10:22:05 +0200

    Add a search box for title only

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,38 @@
 /**
  * Created by leo on 13/10/2016.
  */
-$(function() {
-  $('table[id^=prettyTable]').DataTable({
-    "language": {
-      "lengthMenu": "Afficher _MENU_ enregistrements par page",
-      "zeroRecords": "Aucun enregistrement correspondant",
-      "info": "Page _PAGE_ sur _PAGES_",
-      "infoEmpty": "Aucun enregistrement",
-      "infoFiltered": "(recherche effectuée sur _MAX_ enregistrements)",
-      "paginate": {
-        "first": "Début",
-        "last": "Fin",
-        "next": "Suivant",
-        "previous": "Précédent"
-      },
-      "thousands": " ",
-      "decimal": ",",
-      "search": "Rechercher :",
-      "aria": {
-        "sortAscending": ": trier par ordre croissant",
-        "sortDescending": ": trier par ordre décroissant"
-      }
-    },
-    "order": [[0, "asc"]]
-  });
+$(function () {
+    $('table[id^=prettyTable]').DataTable({
+        "language": {
+            "lengthMenu": "Afficher _MENU_ enregistrements par page",
+            "zeroRecords": "Aucun enregistrement correspondant",
+            "info": "Page _PAGE_ sur _PAGES_",
+            "infoEmpty": "Aucun enregistrement",
+            "infoFiltered": "(recherche effectuée sur _MAX_ enregistrements)",
+            "paginate": {
+                "first": "Début",
+                "last": "Fin",
+                "next": "Suivant",
+                "previous": "Précédent"
+            },
+            "thousands": " ",
+            "decimal": ",",
+            "search": "Rechercher :",
+            "aria": {
+                "sortAscending": ": trier par ordre croissant",
+                "sortDescending": ": trier par ordre décroissant"
+            }
+        },
+        "order": [[0, "asc"]]
+    });
 
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    $('.searchbox-input').change(function () {
+        $('.card').show();
+        var filter = $(this).val(); // get the value of the input, which we filter on
+        $('.container').find(".card-title:not(:contains(" + filter + "))").parent().parent().css('display', 'none');
+    });
 });
