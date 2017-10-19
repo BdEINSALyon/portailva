e1be2925da75a31a53a0556b6055dc8bea5222dc
Gabriel Augendre <gabriel@augendre.info>
2017-10-19 23:41:36 +0200

    Inhibit validation for the moment.

--- portailva/utils/validators.py
+++ portailva/utils/validators.py
@@ -5,15 +5,14 @@ from django.core.exceptions import ValidationError
 
 
 def validate_image_url(url):
-    print('requesting image', url)
-    res = requests.get(url)
-    print('res', res)
-    if 'image' not in res.headers.get('Content-Type'):
-        raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
-                              "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
-                              "type Google Drive mais bien vers le fichier en lui-même. "
-                              "Assurez-vous également que l'accès à l'image ne requière pas "
-                              "d'authentification (mode \"public\" sur PortailVA).")
+    # res = requests.get(url)
+    # if 'image' not in res.headers.get('Content-Type'):
+    #     raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
+    #                           "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
+    #                           "type Google Drive mais bien vers le fichier en lui-même. "
+    #                           "Assurez-vous également que l'accès à l'image ne requière pas "
+    #                           "d'authentification (mode \"public\" sur PortailVA).")
+    pass
 
 
 def validate_iban(iban):
