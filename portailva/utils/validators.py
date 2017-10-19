af2b7df8c86128c3fa451f36103f5a5cc8422b17
Gabriel Augendre <gabriel@augendre.info>
2017-10-19 22:36:31 +0200

    Debugging logo url field

--- portailva/utils/validators.py
+++ portailva/utils/validators.py
@@ -5,7 +5,9 @@ from django.core.exceptions import ValidationError
 
 
 def validate_image_url(url):
+    print('requesting image', url)
     res = requests.get(url)
+    print('res', res)
     if 'image' not in res.headers.get('Content-Type'):
         raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
                               "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
