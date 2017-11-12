40f926fddda3b20c38ab478f31a3dcee33d1064f
Gabriel Augendre <gabriel@augendre.info>
2017-11-12 19:38:47 +0100

    Add more debug messages

--- portailva/utils/forms.py
+++ portailva/utils/forms.py
@@ -26,27 +26,25 @@ class ImageURLField(forms.URLField):
     def clean(self, value):
         value = super().clean(value)
         print('test value', value, flush=True)
-        print('sleeping 2 sec', flush=True)
-        time.sleep(2)
-        print('slept', flush=True)
         if value:
             req = urllib.request.Request(value)
             req.get_method = lambda: 'HEAD'
+            error_message = ("L'URL saisie ne semble pas pointer vers une image valide. "
+                             "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
+                             "type Google Drive mais bien vers le fichier en lui-même. "
+                             "Assurez-vous également que l'accès à l'image ne requière pas "
+                             "d'authentification (mode \"public\" sur PortailVA).")
+            print('req url', req.full_url, flush=True)
+            print('req method', req.get_method(), flush=True)
             try:
-                res = urllib.request.urlopen(req) # type: http.client.HTTPResponse
+                print('before urlopen', flush=True)
+                res = urllib.request.urlopen(req)  # type: http.client.HTTPResponse
+                print('after urlopen', flush=True)
             except urllib.error.HTTPError as err:
-                raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
-                                      "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
-                                      "type Google Drive mais bien vers le fichier en lui-même. "
-                                      "Assurez-vous également que l'accès à l'image ne requière pas "
-                                      "d'authentification (mode \"public\" sur PortailVA).")
+                raise ValidationError(error_message)
             else:
                 print('status_code', res.getcode(), flush=True)
                 print('headers', res.getheaders(), flush=True)
                 if 'image' not in res.getheader('Content-Type'):
-                    raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
-                                          "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
-                                          "type Google Drive mais bien vers le fichier en lui-même. "
-                                          "Assurez-vous également que l'accès à l'image ne requière pas "
-                                          "d'authentification (mode \"public\" sur PortailVA).")
+                    raise ValidationError(error_message)
         return value
