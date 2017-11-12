2096b642d2702d581f74cf57d96d1ed675983a57
Gabriel Augendre <gabriel@augendre.info>
2017-11-12 11:41:54 +0100

    Try to re-enable image validation

--- portailva/utils/validators.py
+++ portailva/utils/validators.py
@@ -5,14 +5,14 @@ from django.core.exceptions import ValidationError
 
 
 def validate_image_url(url):
-    pass
-    # res = requests.get(url)
-    # if 'image' not in res.headers.get('Content-Type'):
-    #     raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
-    #                           "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
-    #                           "type Google Drive mais bien vers le fichier en lui-même. "
-    #                           "Assurez-vous également que l'accès à l'image ne requière pas "
-    #                           "d'authentification (mode \"public\" sur PortailVA).")
+    # pass
+    res = requests.get(url)
+    if 'image' not in res.headers.get('Content-Type'):
+        raise ValidationError("L'URL saisie ne semble pas pointer vers une image valide. "
+                              "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
+                              "type Google Drive mais bien vers le fichier en lui-même. "
+                              "Assurez-vous également que l'accès à l'image ne requière pas "
+                              "d'authentification (mode \"public\" sur PortailVA).")
 
 
 def validate_iban(iban):
