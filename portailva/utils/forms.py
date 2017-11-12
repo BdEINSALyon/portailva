83bbce9994b6caed8132706f95459c603fdfba58
Gabriel Augendre <gabriel@augendre.info>
2017-11-12 20:10:40 +0100

    Disable validation

--- portailva/utils/forms.py
+++ portailva/utils/forms.py
@@ -25,26 +25,26 @@ class PlaceForm(forms.ModelForm):
 class ImageURLField(forms.URLField):
     def clean(self, value):
         value = super().clean(value)
-        print('test value', value, flush=True)
-        if value:
-            req = urllib.request.Request(value)
-            req.get_method = lambda: 'HEAD'
-            error_message = ("L'URL saisie ne semble pas pointer vers une image valide. "
-                             "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
-                             "type Google Drive mais bien vers le fichier en lui-même. "
-                             "Assurez-vous également que l'accès à l'image ne requière pas "
-                             "d'authentification (mode \"public\" sur PortailVA).")
-            print('req url', req.full_url, flush=True)
-            print('req method', req.get_method(), flush=True)
-            try:
-                print('before urlopen', flush=True)
-                res = urllib.request.urlopen(req)  # type: http.client.HTTPResponse
-                print('after urlopen', flush=True)
-            except urllib.error.HTTPError as err:
-                raise ValidationError(error_message)
-            else:
-                print('status_code', res.getcode(), flush=True)
-                print('headers', res.getheaders(), flush=True)
-                if 'image' not in res.getheader('Content-Type'):
-                    raise ValidationError(error_message)
+        # print('test value', value, flush=True)
+        # if value:
+        #     req = urllib.request.Request(value)
+        #     req.get_method = lambda: 'HEAD'
+        #     error_message = ("L'URL saisie ne semble pas pointer vers une image valide. "
+        #                      "Assurez-vous que l'URL que vous fournissez ne pointe pas vers une visionneuse "
+        #                      "type Google Drive mais bien vers le fichier en lui-même. "
+        #                      "Assurez-vous également que l'accès à l'image ne requière pas "
+        #                      "d'authentification (mode \"public\" sur PortailVA).")
+        #     print('req url', req.full_url, flush=True)
+        #     print('req method', req.get_method(), flush=True)
+        #     try:
+        #         print('before urlopen', flush=True)
+        #         res = urllib.request.urlopen(req)  # type: http.client.HTTPResponse
+        #         print('after urlopen', flush=True)
+        #     except urllib.error.HTTPError as err:
+        #         raise ValidationError(error_message)
+        #     else:
+        #         print('status_code', res.getcode(), flush=True)
+        #         print('headers', res.getheaders(), flush=True)
+        #         if 'image' not in res.getheader('Content-Type'):
+        #             raise ValidationError(error_message)
         return value
