374532f46b64def65841c432c104e14ae7c60a42
Gabriel Augendre <gabriel@augendre.info>
2017-12-18 00:08:34 +0100

    Re-use gunicorn as it's not the source of the problem

--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 manage.py migrate && python3 manage.py collectstatic --noinput && python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py migrate && python3 manage.py collectstatic --noinput && gunicorn $WSGI_APP -b 0.0.0.0:8000 --log-file -
