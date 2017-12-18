0704c284ef26ee2cd78db1e1c3f0ddd0987fff70
Gabriel Augendre <gabriel@augendre.info>
2017-12-18 16:53:56 +0100

    Allow bulk event publish from admin

--- portailva/event/admin.py
+++ portailva/event/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from portailva.event.models import EventType, Event, EventPrice
 
+
+def publish(modeladmin, request, queryset):
+    queryset.update(is_online=True)
+
+
+publish.short_description = "Publier les évènements sélectionnés"
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_online', 'type', 'begins_at', 'ends_at']
+    ordering = ['name']
+    actions = [publish]
+
+
 admin.site.register(EventType)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(EventPrice)
